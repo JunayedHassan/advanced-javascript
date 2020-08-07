51e138beb834db56c80118414366d922e10c748f
JunayedHassan <junayed.adarsha@gmail.com>
2020-08-07 09:46:03 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,27 @@
+// const age = 0;
+// if(age){
+//     console.log('condition is true')
+// }
+// else{
+//     console.log('condition is false')
+// }
+
+// const name = '';
+// if(name){
+//     console.log('condition is true')
+// }
+// else{
+//     console.log('condition is false')
+// }
+
+
+// 0, false, '', undefined, null, NaN condition will be false
+// '0', ' ', []
+let name =12 ;
+// console.log(name);
+if(name || name == 0){
+    console.log('condition is true')
+}
+else{
+    console.log('condition is false')
+}
