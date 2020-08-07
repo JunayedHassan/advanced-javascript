7aee813cd6cadf642534c2a4b2b46776ca2b36e1
JunayedHassan <junayed.adarsha@gmail.com>
2020-08-07 10:04:28 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+// let pakhi;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2)
+// }
+
+// const  premik= {name:'smart Dude', phone: 01542};
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
